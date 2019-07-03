f36b561da3bea3cebe3af9ed18c9723b1a1a307e
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2019-07-03 16:01:25 -0300

    Initial Commit

--- RulAR/ViewController.swift
+++ RulAR/ViewController.swift
@@ -12,6 +12,8 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    var dotNodes = [SCNNode]()
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -20,14 +22,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.debugOptions = .showFeaturePoints
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -40,36 +37,100 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touchPoint = touches.first?.location(in: sceneView) {
+            
+            if let hitResult = sceneView.hitTest(touchPoint, types: .featurePoint).first {
+                addDot(at: hitResult)
+            }
+        }
+    }
+    
+    
+    private func addDot(at hitResult: ARHitTestResult) {
+        
+        let dotGeometry = SCNSphere(radius: 0.005)
+        dotGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        
+        dotNode.position = SCNVector3(
+            hitResult.worldTransform.columns.3.x,
+            hitResult.worldTransform.columns.3.y,
+            hitResult.worldTransform.columns.3.z
+        )
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
+        
+        dotNodes.append(dotNode)
+        
+        calculateDistance(dotNodes)
+    }
+    
+    
+    private func calculateDistance(_ nodes: [SCNNode]) {
+        
+        if nodes.count >= 2 {
+            
+            let startPosition = nodes.first!.position
+            let endPosition = nodes.last!.position
+            
+            let distance = sqrt(
+                pow(endPosition.x - startPosition.x, 2) +
+                pow(endPosition.y - startPosition.y, 2) +
+                pow(endPosition.z - startPosition.z, 2)
+            )
+   
+            printText(text: "\(Int(distance*100)) cm", atPosition: endPosition)
+            
+            drawLine(from: startPosition, to: endPosition)
+            
+            dotNodes.removeFirst()
+        }
+    }
+    
+    private func printText(text: String, atPosition position: SCNVector3) {
+    
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        let textNode = SCNNode(geometry: textGeometry)
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+    }
+    
+    
+    private func drawLine(from startPosition: SCNVector3, to endPosition: SCNVector3) {
+        
+        let lineGeometry = SCNGeometry.line(from: startPosition, to: endPosition)
+        
+        let lineNode = SCNNode(geometry: lineGeometry)
+        lineNode.position = SCNVector3Zero
+        
+        sceneView.scene.rootNode.addChildNode(lineNode)
+    }
+    
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
         sceneView.session.pause()
     }
+}
 
-    // MARK: - ARSCNViewDelegate
+extension SCNGeometry {
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    class func line(from vector1: SCNVector3, to vector2: SCNVector3) -> SCNGeometry {
         
+        let indices: [Int32] = [0, 1]
+        let source = SCNGeometrySource(vertices: [vector1, vector2])
+        let element = SCNGeometryElement(indices: indices, primitiveType: .line)
+        
+        return SCNGeometry(sources: [source], elements: [element])
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
